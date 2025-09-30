36bfe3d6320b4bfa763ad0c3c2d8a12cdbe8a1aa
Mark Hazleton <mark.hazleton@controlorigins.com>
2025-09-30 08:18:47 -0500

    feat: update version to 1.0.5 and enhance .gitignore for .NET binaries

---
+++ bin/git-spark.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+// Adjusted paths: TypeScript compilation preserves 'src' directory in dist (dist/src/...)
+// so we require from dist/src to locate emitted JS modules.
+const { createCLI } = require('../dist/src/cli/commands');
+const { setGlobalLogLevel } = require('../dist/src/utils/logger');
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+
+// Set default log level
+setGlobalLogLevel('info');
+
+// Create and run CLI
+const program = createCLI();
+program.parse(process.argv);
